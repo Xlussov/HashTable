d4c9db6d250b4ba78d52c16d7a7df5e246bfa62e
Xlussov <130926999+Xlussov@users.noreply.github.com>
2025-10-24 11:17:34 +0300

    deleted unused code

--- main.go
+++ main.go
@@ -8,34 +8,30 @@ import (
 
 func main() {
 	ht := hashtable.New[string, int](8)
-	ht.Put("alah1", 1)
-	ht.Put("alah2", 2)
-	ht.Put("alah3", 3)
-	ht.Put("alah4", 4)
-	ht.Put("alah5", 5)
-	ht.Put("alah6", 6)
-	ht.Put("alah7", 7)
+
+	ht.Put("value1", 1)
+	ht.Put("value2", 2)
+	ht.Put("value3", 3)
+	ht.Put("value4", 4)
+	ht.Put("value5", 5)
+	ht.Put("value6", 6)
+	ht.Put("value7", 7)
 	ht.Print()
 	fmt.Println("==============================")
 
-	// ht.Put("alah7", 7)
-	ht.Put("alah8", 8)
+	ht.Put("value8", 8)
 
-	// if v, ok := ht.Get("alah1"); ok {
-	// 	fmt.Println(v)
-	// }
+	if v, ok := ht.Get("value1"); ok {
+		fmt.Println(v)
+	}
 
-	// if v, ok := ht.Remove("alah1"); ok {
-	// 	fmt.Println(v)
-	// }
+	if v, ok := ht.Remove("value1"); ok {
+		fmt.Println(v)
+	}
 
-	// if v, ok := ht.Get("alah1"); ok {
-	// 	fmt.Println(v)
-	// }
+	if v, ok := ht.Get("value1"); ok {
+		fmt.Println(v)
+	}
 
 	ht.Print()
-
-	// ht.Put("alah1", 1488)
-
-	// ht.Print()
 }
